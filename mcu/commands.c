556407fae072d105fe75dfc6d1c0ced4cf387e6f
Benjamin Medicke <bmedicke@gmail.com>
2018-10-22 05:46:33 +0200

    add commands for first 10 profiles

--- mcu/commands.c
+++ mcu/commands.c
@@ -13,6 +13,36 @@ void handle_commands() {
         global_settings_send();
         profile_send(global_settings_get().active_profile);
         break;
+      case '0':
+        profile_send(0);
+        break;
+      case '1':
+        profile_send(1);
+        break;
+      case '2':
+        profile_send(2);
+        break;
+      case '3':
+        profile_send(3);
+        break;
+      case '4':
+        profile_send(4);
+        break;
+      case '5':
+        profile_send(5);
+        break;
+      case '6':
+        profile_send(6);
+        break;
+      case '7':
+        profile_send(7);
+        break;
+      case '8':
+        profile_send(8);
+        break;
+      case '9':
+        profile_send(9);
+        break;
       default:
         uart_transmit('?');
     }
