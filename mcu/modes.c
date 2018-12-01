849626c0b90f2ec6fd0c4e858b1df06778ed3a36
namanuel <nagel.manuel@gmx.at>
2018-12-01 17:41:54 +0100

    start to make intervalometer dynamic

--- mcu/modes.c
+++ mcu/modes.c
@@ -52,36 +52,57 @@ void _mode_step_shoot_step(uint32_t second) {
   local_persist uint32_t last_second = 0;
 //eeprom einlesen
   /* entered once per second: */
+
+  //Endstops
+  DDRD |= (1 << PD2) | (1 << PD3); // input.
+  PORTD |= (1 << PD2) | (1 << PD3); // enable pullup.
+
   if (last_second != second) {
   }
+  global_settings global = global_settings_get();
 
+
+
+  profile p;
+  if(global_settings.active_profile() == 1) {//for SSS mode
+    profile_get(&p, global_settings.active_profile());
+  }
   while(1){
     //TODO
     //von eeprom lesen?
     //todo endstop = richtungswechsel oder stop
     //notlösung: abfragen ob notstop erreicht wurde !!!! vor schritt 1
     //variabel machen
+
+    if (PIND & (1 << PD2)){
+      //or other change direction
+
+      _delay_ms(300);
+    }
+
   // 1. step:
+  _delay_ms(p.startup_delay());
+  if(p.direction() == "left")//if which direction set stepper in the other direction
   for (int i = 0; i < 50; i++) {
-    _delay_us(1);
+    _delay_us(p.step_delay());
     PIND |= (1 << PD7); // toggle motor.
-    _delay_us(1);
+    _delay_us(p.step_delay());
   }
   PORTD &= ~(1 << PD7);
 
   // 2. wait for vibrations to settle:
-  _delay_ms(3800);
+  _delay_ms(p.cooldown());//todo cooldown
 
   // 3. trigger camera:
   {
     DDRC |= (1 << PC1);
     PORTC |= (1 << PC1);
-    _delay_ms(200);
+    _delay_ms(200);//hardcoded
     PORTC &= ~(1 << PC1);
   }
 
   // 4. wait for camera:
-  _delay_ms(5000);
+  _delay_ms(5000);//todo
 
 }
   last_second = second;
