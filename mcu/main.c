a1ef5cff3160eee257ec56eb6db82c5066f47ce0
Benjamin Medicke <bmedicke@gmail.com>
2018-12-10 07:31:43 +0100

    add comments

--- mcu/main.c
+++ mcu/main.c
@@ -27,18 +27,19 @@ void slider_init() {
   PORTD &= ~(1 << PD5); /* enable driver by pulling /enable low. */
 
   /* joystick button: */
-  DDRC |= (1 << PC2); // input.
-  PORTC |= (1 << PC2); // enable pullup.
+  DDRC |= (1 << PC2); /* input. */
+  PORTC |= (1 << PC2); /* enable pullup. */
 
   /* endstops: */
-  DDRD &= ~((1 << PD2) | (1 << PD3)); // input. irgendwo falsch
-  PORTD |= (1 << PD2) | (1 << PD3); // enable pullup.
+  DDRD &= ~((1 << PD2) | (1 << PD3)); /* input. irgendwo falsch */
+  PORTD |= (1 << PD2) | (1 << PD3); /* enable pullup. */
 
   /* camera trigger: */
   DDRC |= (1 << PC1);
 }
 
 int main() {
+  /* initialize special function registers: */
   uart_init(UART_BAUD);
   external_eeprom_init(I2C_SCL);
   timer_init();
@@ -46,16 +47,19 @@ int main() {
   slider_init();
   sei();
 
+  /* wait a few seconds for programming instructions: */
   block_for_programming_mode_window(&second);
 
+  /* read global and profile settings from internal eeprom: */
   global_settings global = global_settings_get();
-
   profile p;
   if (!profile_get(&p, global.active_profile))
     error_string("profile id out of range!");
 
   for (;;) {
+    /* check for and handle UART commands: */
     handle_commands();
+    /* run configured mode: */
     handle_mode(p.drive_mode, second);
   }
 }
